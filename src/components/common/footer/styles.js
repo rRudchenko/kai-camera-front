d7ac421eec94069e9806deffdc155871a16552b0
Vadym Mykhalets <vm@pozitiff.ua>
2019-08-23 12:12:28 +0000

    Frankie/master design fixes

--- src/components/common/footer/styles.js
+++ src/components/common/footer/styles.js
@@ -2,28 +2,31 @@ import styled from 'styled-components'
 
 export const MainContainer = styled.section`
   background: #fafafa;
-  width: 100%;
+  max-width: 100%;
 `
 
 export const FooterContainer = styled.footer`
-  width: 100%;
   padding: 0;
   margin: 0;
+  width: 100%;
+  background: #f9c806;
 `
 
 export const TextContainer = styled.div`
-  background: #f9c806;
+  margin: 0 auto;
+  box-sizing: border-box;
+  max-width: 935px;
   padding: 20px;
 `
 
 export const Cameras = styled.div`
   width: 100%;
   height: 100px;
-  background-size: 100px 40px;
-  background-image: url('/icons/camera.svg'), url('/icons/camera.svg'),
-    url('/icons/camera.svg'), url('/icons/camera.svg');
-  background-position: top 0 left 0, top 20px left 50px, top 40px left 0,
-    top 60px left 50px;
+  background-size: 86px 74px;
+  background-image: url('/icons/cameranew.png'), url('/icons/cameranew.png'),
+    url('/icons/cameranew.png'), url('/icons/cameranew.png');
+  background-position: left top, left 129px top 15px, left 0 top 37px,
+    left 129px top 52px;
   background-repeat: repeat-x;
 `
 
